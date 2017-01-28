75e667a9ce4108bc15b52bf4cb6d0c929e115a6f
Rafaela <rafaela.vm@gmail.com>
2017-01-28 12:22:35 -0200

    3. Separação da lógica e visualização em camadas. Criação do controller ListagemController

---
+++ www/js/controllers/ListagemController.js
@@ -0,0 +1,6 @@
+angular.module('starter')
+.controller('ListagemController', function($scope){
+
+    $scope.listaDeCarros = ['BMW 120i', 'Onix 1.6', 'Fiesta 2.0', 'Celta 2008 1.0', 'Astra Sedan', 'Vectra 2.0 Turbo'
+    ];
+});
