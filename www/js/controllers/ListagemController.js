50c348a3493ff56f0c5ad6abe80999bc13db6fe6
Rafaela <rafaela.vm@gmail.com>
2017-02-09 16:42:47 -0200

    5 (1) Adicionando acessórios dos carros com toggles.Adicionando o preço dos carros na lista

--- www/js/controllers/ListagemController.js
+++ www/js/controllers/ListagemController.js
@@ -1,6 +1,9 @@
 angular.module('starter')
 .controller('ListagemController', function($scope){
 
-    $scope.listaDeCarros = ['BMW 120i', 'Onix 1.6', 'Fiesta 2.0', 'Celta 2008 1.0', 'Astra Sedan', 'Vectra 2.0 Turbo'
-    ];
+    $scope.listaDeCarros = [{
+        'nome': 'BMW 120i',
+        'preco': 70.000
+
+    }];
 });
--- www/js/controllers/carroEscolhidoController.js
+++ www/js/controllers/carroEscolhidoController.js
@@ -1,5 +1,6 @@
 angular.module('starter')
 .controller('carroEscolhidoController', function($scope, $stateParams){
 
-    $scope.carroEscolhido = $stateParams.carro;
+    //$stateParams.carro vem como string, então tem que transformar em json
+    $scope.carroEscolhido = angular.fromJson($stateParams.carro);
 });
