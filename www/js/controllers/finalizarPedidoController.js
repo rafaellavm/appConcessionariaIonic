bd98c7bfd59b7bf29f7d44f87e8c6272989be5bf
Rafaela <rafaela.vm@gmail.com>
2017-02-20 16:52:13 -0300

    6 (3) Finalizando o pedido e o pop-up. Pop-up de finalização

--- www/js/controllers/finalizarPedidoController.js
+++ www/js/controllers/finalizarPedidoController.js
@@ -1,7 +1,18 @@
 angular.module('starter')
-.controller('finalizarPedidoController', function($stateParams, $scope){
+.controller('finalizarPedidoController', function($stateParams, $scope, $ionicPopup, $state){
 
 // $stateParams: recebe o parametro
     $scope.carroFinalizado = angular.fromJson($stateParams.carro);
+    
+    $scope.finalizarPedido = function(){
+
+        $ionicPopup.alert({
+            title: "Parabéns!",
+            template: "Você acaba de comprar um carro!"
+        }).then(function(){
+            $state.go('listagem')
+        });
+    }
+
 
 });
