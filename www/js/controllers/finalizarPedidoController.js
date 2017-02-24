e2f4b6129f91a47c3f254fa8dc5d235b4a1c8039
Rafaela <rafaela.vm@gmail.com>
2017-02-24 20:14:57 -0300

    7 (2) Conectando App com serviço externo. Salvando o pedido

--- www/js/controllers/finalizarPedidoController.js
+++ www/js/controllers/finalizarPedidoController.js
@@ -1,18 +1,41 @@
 angular.module('starter')
-.controller('finalizarPedidoController', function($stateParams, $scope, $ionicPopup, $state){
+.controller('finalizarPedidoController', function($stateParams, $scope, $ionicPopup, $state, CarroService){
 
 // $stateParams: recebe o parametro
     $scope.carroFinalizado = angular.fromJson($stateParams.carro);
+    $scope.pedido = {};
     
-    $scope.finalizarPedido = function(){
+    $scope.finalizarPedido = function(pedido){
 
-        $ionicPopup.alert({
-            title: "Parabéns!",
-            template: "Você acaba de comprar um carro!"
-        }).then(function(){
-            $state.go('listagem')
+        var pedidoFinalizado = {
+            params:{
+                carro: $scope.carroFinalizado.nome,
+                preco: $scope.carroFinalizado.preco,
+                nome: $scope.pedido.nome,
+                endereco: $scope.pedido.endereco,
+                email: $scope.pedido.email
+            }
+        }
+
+        console.log('pedido = ', pedidoFinalizado);
+        CarroService.salvarPedido(pedidoFinalizado).then(function(dados){
+            
+            $ionicPopup.alert({
+                title: "Parabéns!",
+                template: "Você acaba de comprar um carro!"
+            }).then(function(){
+                $state.go('listagem')
+            });
+
+        }, function(erro){
+             
+             $ionicPopup.alert({
+                title: "Deu erro",
+                template: "Campos obrigatórios"
+            });
+
         });
-    }
 
+    }
 
 });
--- www/js/services/service.js
+++ www/js/services/service.js
@@ -6,8 +6,13 @@ angular.module('starter')
     return {
         obterCarros: function(){
             return $http.get(url).then(function(response){
-                console.log('response.data = ', response.data);
+                //console.log('response.data = ', response.data);
                 return response.data;
+            });
+        },
+        salvarPedido: function(pedido){
+            return $http.get(url + 'salvarpedido', pedido).then(function(response){
+                return 'Deu certo';
             });
         }
     }
